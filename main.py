87236a59235df809f17d265d734531a1a7ec13c5
Martin Reynolds <62625037+martinbreynolds@users.noreply.github.com>
2022-10-11 15:13:18 +0000

    Check if letter user has input matches the letter in the random word and replace _ with letter.

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import random
 
+#Random Word List
 word_list = ["aardvark", "baboon", "camel"]
 
-chosen_word = word_list[random.randint(0,len(word_list)-1)]
+# Chose a Random Word
+chosen_word = word_list[random.randint(0, len(word_list) - 1)]
 print(chosen_word)
 
+# Display '_' for each letter in the word randomly chosen
 display = []
 for letter in chosen_word:
-  display.append('_')
+    display += '_'
+print(display)
 
-guess = input("Guess a letter:  ").lower()
+# Ask the user for a letter
+guessed_letter = input("Guess a letter:  ").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# Check if letter user has input matches the letter in the random word and replace _ with letter.
 
-for letter in chosen_word:
-  if guess == letter:
-    print("Right")
-  else:
-    print("Wrong")
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if guessed_letter == letter:
+        display[position] = letter
 
 print(display)
