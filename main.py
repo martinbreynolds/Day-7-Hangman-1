b33ef3fee3be0970b403523fc8ba47ec56b0dd40
Martin Reynolds <62625037+martinbreynolds@users.noreply.github.com>
2022-10-11 14:43:45 +0000

    Display _ for each letter

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-#Step 1 
 import random
+
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = word_list[random.randint(0,len(word_list)-1)]
+print(chosen_word)
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+display = []
+for letter in chosen_word:
+  display.append('_')
 
 guess = input("Guess a letter:  ").lower()
 
@@ -16,3 +18,5 @@ for letter in chosen_word:
     print("Right")
   else:
     print("Wrong")
+
+print(display)
